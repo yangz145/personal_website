1e85e39f69e7d138f1b8efd7330d7dca53068a51
yangz145 <2224247977a@gmail.com>
2025-09-10 00:28:35 -0400

    editions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import projImg3 from "../assets/img/sample2.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import "react-multi-carousel/lib/styles.css";
 
 export const Projects = () => {
 
